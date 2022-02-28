8b0bf424afa0c3f7cae8765ee68c2c6b08573cd5
MeatChips <84376178+MeatChips@users.noreply.github.com>
2022-02-28 12:21:45 +0100

    program.cs added

---
+++ GhostGen/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GhostGen
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello");
+			AsteroidGame game = new AsteroidGame();
+			game.Play();
+		}
+	}
+}
